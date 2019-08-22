8eefd3369330af8e12325dab1d00d7d265941bb1
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-22 01:13:59 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(file)
+end
+def my_each(file)
+   my_each(words)  do word
+  file = file.read("while")
+   my_each(file)
+ end
+   
+def my_each(words)
+  puts my_each(words) do |item|
+  words = "(words, [counter])"
+  counter 0
+  words = "(words, [counter])"
+  counter+=1
+  end
 end
